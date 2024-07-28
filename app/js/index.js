b2491fa560be20b36ef0185b626cdeb4e5c4bb06
sh4vyar <wolfkurd66@gmail.com>
2024-07-29 00:11:29 +0300

    bye bye :]

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,25 @@
 let items = document.querySelectorAll(".item");
 let content = document.querySelector(".content");
-const contents = {
-  Home: `<div>hi</div>`,
-  Idk: `<div>Idk</div>`,
-  Stillduno: `<div>stillduno</div>`,
-};
-if (!content.innerHTML) {
-  content.innerHTML = contents.Home;
-}
+let inputNumber = document.querySelector('.cheat-item > input[type="number"]');
+let inputRange = document.querySelector('.cheat-item > input[type="range"]');
+
+inputNumber.value = 30;
+
+inputNumber.addEventListener("input", () => {
+  inputRange.value = inputNumber.value;
+});
+inputRange.addEventListener("input", () => {
+  inputNumber.value = inputRange.value;
+});
+
+// const contents = {
+//   Home: `<div>hi</div>`,
+//   Idk: `<div>Idk</div>`,
+//   Stillduno: `<div>stillduno</div>`,
+// };
+// if (!content.innerHTML) {
+//   content.innerHTML = contents.Home;
+// }
 
 const home = document.getElementById("0");
 const idk = document.getElementById("1");
@@ -25,8 +37,8 @@ stillduno.addEventListener("click", () => {
   content.innerHTML = contents.Stillduno;
 });
 
-home.classList.add("item-active");
-let selectedItem = document.querySelector(".item-active");
+home.classList.add("active");
+let selectedItem = document.querySelector(".active");
 
 items.forEach((i) => {
   toggleActive(i);
@@ -36,13 +48,13 @@ function toggleActive(item) {
   item.addEventListener("click", (e) => {
     if (selectedItem) {
       removeActive(items);
-      e.target.classList.add("item-active");
+      e.target.classList.add("active");
     }
   });
 
   function removeActive(allItems) {
     allItems.forEach((item) => {
-      item.classList.remove("item-active");
+      item.classList.remove("active");
     });
   }
 }
